4e1e36650a4b1d1b513a7be7feb8e79459fddd4f
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-08 09:43:00 -0500

    NTS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,9 +21,9 @@
 </head>
 
 <body class="relative min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
-  style="background-image: url(/assets/img/background-home.jpg)">
+  style="background-image: url(/assets/img/bg-atalaya.jpg)">
   <main class="absolute top-0 left-0 bottom-0 right-0" style="backdrop-filter: blur(40px)">
-    <div class="absolute top-4 md:top-16 bottom-4 md:bottom-16 left-8 md:left-32 right-8 md:right-32 bg-cover bg-center bg-no-repeat rounded-sm shadow-2xl flex items-center justify-center flex-col gap-8" style="background-image: url(/assets/img/background-home.jpg)">
+    <div class="absolute top-4 md:top-16 bottom-4 md:bottom-16 left-8 md:left-32 right-8 md:right-32 bg-cover bg-center bg-no-repeat rounded-sm shadow-2xl flex items-center justify-center flex-col gap-8" style="background-image: url(/assets/img/bg-atalaya.jpg)">
       <h1 class="text-3xl font-bold text-white" style="text-shadow: 0 0 40px #000">Bienvenido a Atalaya</h1>
       <div class="space-x-4">
         <a href="/login" class="px-4 py-2 rounded-full">Iniciar sesión</a>
