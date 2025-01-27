e517ac5e7c159ec992b215afc880d86532756147
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-27 12:32:41 -0500

    Dando formato al proximanente

--- resources/js/Services.jsx
+++ resources/js/Services.jsx
@@ -185,7 +185,7 @@ const Services = ({ businesses = [], services = [], session, APP_DOMAIN, APP_PRO
                               display: 'block',
                               backgroundColor: '#f9c851',
                               color: '#343a40',
-                              padding: '.28rem 1.28rem',
+                              padding: '.28rem 1.56rem',
                               width: 'max-content',
                               borderRadius: '0 25px 25px 0',
                             }}>Proximamente</span>
