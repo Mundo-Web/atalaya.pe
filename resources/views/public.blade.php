d08a7cb0c5681d1abd3fc778ce0664576c25599b
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-02-21 15:42:47 -0500

    Agregando nueva landing: mas descripcion

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -12,6 +12,8 @@
 
   <title>Atalaya</title>
 
+  <link rel="shortcut icon" href="/assets/img/icon.svg" type="image/png">
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link
